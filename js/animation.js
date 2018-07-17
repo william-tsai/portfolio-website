362db925d014d5a67d4258222cbf44d6b644b8b1
William_Tsai <tsaiyuhlin@gmail.com>
2018-07-17 04:47:21 -0700

    minor change in animation.js

--- js/animation.js
+++ js/animation.js
@@ -12,11 +12,11 @@ $(document).ready(function(){
             scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
     });
     $('#profile-pic-link').on('click', function(){
-        $('.body-tag, .div-tag, .h1-tag, .e-h1-tag, .img-tag, .e-div-tag, .e-body-tag').fadeOut(function(){
+        $('.body-tag, .div-tag, .h1-tag, .e-h1-tag, .img-tag, .e-div-tag, .e-body-tag, .e-html-tag').fadeOut(function(){
             $('.headline-row').css('margin-top', '24px');
             $('#profile-pic').css({'display': 'inline'});
             $('#profile-pic').animate({left: '18vw'});
-            $('.headline-col').animate({left: '15vw'});
+            $('.headline-col').animate({left: '12vw'});
             $('.headline-col').css('max-width', '50vw');
         });
     })
