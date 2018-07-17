ea62dd9ccd6ebadf0cb2399d6020154dc6a33238
William_Tsai <tsaiyuhlin@gmail.com>
2018-07-17 04:42:53 -0700

    first commit

---
+++ js/animation.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".header, .container").height($(window).height());
+    $(".navbar a").click(function(){
+        $("body, html").animate({
+            scrollTop:$("#" + $(this).data('value')).offset().top
+        }, 1000)
+    });
+    AOS.init();
+    $('#scroll-btn').on('click', function(event){ 
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+    $('#profile-pic-link').on('click', function(){
+        $('.body-tag, .div-tag, .h1-tag, .e-h1-tag, .img-tag, .e-div-tag, .e-body-tag').fadeOut(function(){
+            $('.headline-row').css('margin-top', '24px');
+            $('#profile-pic').css({'display': 'inline'});
+            $('#profile-pic').animate({left: '18vw'});
+            $('.headline-col').animate({left: '15vw'});
+            $('.headline-col').css('max-width', '50vw');
+        });
+    })
+})
